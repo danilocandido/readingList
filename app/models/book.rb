50831b13768668595689ea75585c989b941257c5
Danilo Cândido <danilod07a@gmail.com>
2017-07-26 00:10:45 -0300

    adding scopes finished and recent to Book.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,9 @@
 class Book < ApplicationRecord
 
+	scope :finished, -> { where.not(finished_on: nil) }
+	#scope :finished, -> { where('finished_on IS NOT NULL') }
+	scope :recent, -> { where('finished_on > ?', 2.days.ago) }
+
 	def finished?
 		finished_on.present?
 	end
