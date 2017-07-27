bc832da8e6fdaade5a917346800f52275c8a0564
Danilo Cândido <danilod07a@gmail.com>
2017-07-27 01:17:41 -0300

    added keywords to books and finished the search of books with keywords

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,7 +4,9 @@ class Book < ApplicationRecord
 	scope :finished, -> { where.not(finished_on: nil) }
 	#scope :finished, -> { where('finished_on IS NOT NULL') }
 	scope :recent, -> { where('finished_on > ?', 2.days.ago) }
-	scope :search, -> (keyword) { where(title: keyword) if keyword.present? }
+	scope :search, -> (keyword) { where('keywords LIKE ?', "%#{keyword.downcase}%") if keyword.present? }
+
+	before_save :set_keywords
 
 	# A scope always return a collection a class method could return nil
 	#def self.search(keyword)
@@ -18,4 +20,10 @@ class Book < ApplicationRecord
 	def finished?
 		finished_on.present?
 	end
+
+	protected
+		#this will helf the search of books
+		def set_keywords
+			self.keywords = [title, author, description].map(&:downcase).join(' ')
+		end
 end
