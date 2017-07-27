4122b3c5e8c93e556050d01dd67f367849d4eaaa
Danilo Cândido <danilod07a@gmail.com>
2017-07-27 00:41:28 -0300

    Search title

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,19 @@
 class Book < ApplicationRecord
 
+	
 	scope :finished, -> { where.not(finished_on: nil) }
 	#scope :finished, -> { where('finished_on IS NOT NULL') }
 	scope :recent, -> { where('finished_on > ?', 2.days.ago) }
+	scope :search, -> (keyword) { where(title: keyword) if keyword.present? }
+
+	# A scope always return a collection a class method could return nil
+	#def self.search(keyword)
+	#	if keyword.present?
+	#		where(title: keyword)
+	#	else
+	#		all
+	#	end
+	#end
 
 	def finished?
 		finished_on.present?
